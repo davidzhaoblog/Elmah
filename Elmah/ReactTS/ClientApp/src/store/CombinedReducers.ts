db90c9046067d2ceaf7d354bf864379850503ea6
David Zhao <david.zhao.blog@gmail.com>
2021-03-18 10:56:41 -0400

    React Typescript: listSclices.

--- Elmah/ReactTS/ClientApp/src/store/CombinedReducers.ts
+++ Elmah/ReactTS/ClientApp/src/store/CombinedReducers.ts
@@ -5,28 +5,24 @@ import auth from "src/features/Authentication/authenticationSlice";
 import todos from 'src/features/Todo/todoSlice';
 import visibilityFilter from 'src/features/visibilityFilter/visibilityFilterSlice';
 
-
 import eLMAH_Error from 'src/features/ELMAH_Error/Slice';
-
-
 import elmahApplication from 'src/features/ElmahApplication/Slice';
-
-
 import elmahHost from 'src/features/ElmahHost/Slice';
-
-
 import elmahSource from 'src/features/ElmahSource/Slice';
-
-
 import elmahStatusCode from 'src/features/ElmahStatusCode/Slice';
-
-
 import elmahType from 'src/features/ElmahType/Slice';
-
-
 import elmahUser from 'src/features/ElmahUser/Slice';
 
 
+import { 
+    eLMAH_ErrorList,
+    elmahApplicationList,
+    elmahHostList,
+    elmahSourceList,
+    elmahStatusCodeList,
+    elmahTypeList,
+    elmahUserList
+ } from 'src/features/listSlices';
 
 export const reducers = combineReducers({
     app: app,
@@ -35,28 +31,21 @@ export const reducers = combineReducers({
     todos: todos,
     visibilityFilter: visibilityFilter,
 
-
     eLMAH_Error: eLMAH_Error,
-
-
     elmahApplication: elmahApplication,
-
-
     elmahHost: elmahHost,
-
-
     elmahSource: elmahSource,
-
-
     elmahStatusCode: elmahStatusCode,
-
-
     elmahType: elmahType,
-
-
     elmahUser: elmahUser,
 
-
+    eLMAH_ErrorList,
+    elmahApplicationList,
+    elmahHostList,
+    elmahSourceList,
+    elmahStatusCodeList,
+    elmahTypeList,
+    elmahUserList
 });
 
 export type RootState = ReturnType<typeof reducers>
