1c428809bde8ca6b22b59a811bef7596f0511855
David Zhao <david.zhao.blog@gmail.com>
2021-03-19 11:50:43 -0400

    Update Route.tsx

--- Elmah/ReactTS/ClientApp/src/features/ELMAH_Error/Route.tsx
+++ Elmah/ReactTS/ClientApp/src/features/ELMAH_Error/Route.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Switch } from 'react-router';
 import { PrivateRoute } from '../Authentication/PrivateRoute';
-// import ListPage from 'src/features/ELMAH_Error/ListPage';
+import IndexPage from 'src/features/ELMAH_Error/Index';
 import DetailsPage from './DetailsPage';
 
 export default function ELMAH_ErrorRoute(): JSX.Element {
 	// <PrivateRoute path="/elmah_error" exact={true} component={ListPage} />
     return (<Switch>
+        <PrivateRoute path="/elmah_error" exact={true} component={IndexPage} />
         <PrivateRoute path="/elmah_error/details/:errorId" exact={true} component={DetailsPage} />
     </Switch>);
 }
