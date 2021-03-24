b7f234be048f416542d7eb60409548ba20d88989
david zhao <david.zhao.blog@gmail.com>
2021-03-24 12:49:04 -0400

    React Typescript is ready.

--- Elmah/ReactTS/Startup.cs
+++ Elmah/ReactTS/Startup.cs
@@ -146,7 +146,7 @@ namespace Elmah.MvcCore
             // Add Cors
             services.AddCors(o => o.AddPolicy("AllowAll", builder =>
             {
-                builder.WithOrigins("http://localhost", "https://localhost")
+                builder.WithOrigins("http://localhost:7813", "https://localhost:7814", "http://localhost:3456")
                        //.AllowAnyOrigin()
                        .AllowAnyMethod()
                        .AllowAnyHeader()
