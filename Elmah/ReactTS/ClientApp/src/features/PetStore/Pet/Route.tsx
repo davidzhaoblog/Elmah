8a702376aa2a1e63fb85f3acba958859e060167d
David Zhao <david.zhao.blog@gmail.com>
2021-06-04 11:20:42 -0400

    Swagger PetStore/Pet/Route, default PrivateRoute.

--- Elmah/ReactTS/ClientApp/src/features/PetStore/Pet/Route.tsx
+++ Elmah/ReactTS/ClientApp/src/features/PetStore/Pet/Route.tsx
@@ -9,6 +9,7 @@ import FindPetsByTagsPage from './FindPetsByTagsPage';
 export default function PetRoute(): JSX.Element {
 	// <PrivateRoute path="/pet" exact={true} component={ListPage} />
     return (<Switch>
+		<PrivateRoute path='/PetStore/Pet' exact={true} component={FindPetsByStatusPage} />
 		<PrivateRoute path='/PetStore/Pet/GetPetById/:petId' exact={true} component={GetPetByIdPage} />
 		<PrivateRoute path='/PetStore/Pet/FindPetsByStatus' exact={true} component={FindPetsByStatusPage} />
 		<PrivateRoute path='/PetStore/Pet/FindPetsByTags' exact={true} component={FindPetsByTagsPage} />
