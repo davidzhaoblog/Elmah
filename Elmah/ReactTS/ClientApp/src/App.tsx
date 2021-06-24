bd8bc6c07394e1a3886b632fa690ebbc339630f6
David Zhao <david.zhao.blog@gmail.com>
2021-06-24 16:22:56 -0400

    React17MUI4 Step-1.2.2. material-ui working with AppBar

--- Elmah/ReactTS/ClientApp/src/App.tsx
+++ Elmah/ReactTS/ClientApp/src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Copyright() {
   return (
@@ -19,8 +36,24 @@ function Copyright() {
 }
 
 export default function App() {
+  const classes = useStyles();
   return (
-    <Container maxWidth="sm">
+    // For full screen: use following
+    // <div className={classes.root}></div>
+    // For size lg/md/sm/sx: use following
+    // <Container maxWidth="lg"></Container>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Box my={4}>
       <Typography variant="h4" component="h1" gutterBottom>
           React 17.0.2
@@ -31,6 +64,6 @@ export default function App() {
         <ProTip />
         <Copyright />
       </Box>
-    </Container>
+      </div>
   );
 }
