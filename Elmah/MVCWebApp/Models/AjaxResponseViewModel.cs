4268ccb3db93cb9ec68a3140ce25f58e025fc9fd
David Zhao <david.zhao.blog@gmail.com>
2022-07-02 04:59:54 -0400

    .Net 6.19. Re-developed button-group.

--- Elmah/MVCWebApp/Models/AjaxResponseViewModel.cs
+++ Elmah/MVCWebApp/Models/AjaxResponseViewModel.cs
@@ -4,9 +4,7 @@ namespace Elmah.MvcWebApp.Models
     {
         public System.Net.HttpStatusCode Status { get; set; }
         public string? Message { get; set; }
-
         public string? RequestId { get; set; }
-
         public bool ShowRequestId { get; set; }
     }
 }
