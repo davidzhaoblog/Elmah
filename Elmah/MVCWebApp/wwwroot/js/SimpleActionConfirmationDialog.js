51cb45923bec7009edf4fbcb272c85228ed799d4
David Zhao <david.g.zhao@ontario.ca>
2022-07-24 12:57:12 -0400

    Update SimpleActionConfirmationDialog.js

--- Elmah/MVCWebApp/wwwroot/js/SimpleActionConfirmationDialog.js
+++ Elmah/MVCWebApp/wwwroot/js/SimpleActionConfirmationDialog.js
@@ -25,7 +25,6 @@
  */
 $(document).ready(function () {
     attachSimpleActionConfirmationDialog();
-    attachSimpleActionConfirmationDialogEventHandler();
 })
 
 function attachSimpleActionConfirmationDialog() {
